2a9394a55d24df6dfb0c5230db30b609610ac13b
betty <ibtissambassoma84@gmail.com>
2025-08-30 21:48:42 +0100

    ft_fibonacci

---
+++ ex04/ft_fibonacci.c
@@ -0,0 +1,15 @@
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index <= 1)
+		return (index);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
+
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	printf("%d\n", ft_fibonacci(102));
+// 	return (0);
+// }
