71a9bb30a5cac3df7a96acea50fa726a0c958143
betty <ibtissambassoma84@gmail.com>
2025-08-30 21:05:05 +0100

    factorial done

--- ex00/ft_iterative_factorial.C
+++ ex00/ft_iterative_factorial.C
@@ -1,4 +1,22 @@
-int ft_iterative_factorial(int nb)
+#include <stdio.h>
+
+int	ft_iterative_factorial(int nb)
 {
-    
+	int result;
+
+	if (nb < 0)
+	{
+		return (0);
+	}
+	if (nb <= 1)
+	{
+		return (1);
+	}
+	result = nb;
+	while (nb > 1)
+	{
+		result *= nb - 1;
+		nb--;
+	}
+	return (result);
 }
